ab18655579877af1ecd00d745674d5efac8fb2ef
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2023-09-08 01:13:17 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+// getCurrentTime(): Promise<number, Error>
+// Get the current playback position in seconds.
+
+// player.getCurrentTime().then(function(seconds) {
+// seconds = the current playback position
+// }).catch(function(error) {
+// an error occurred
+// });
+
+localStorage.setItem('videoplayer-current-time', 'playtime');
